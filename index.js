d8c1093bd1b8b3e2e4fdf659a6f9bb9cb679deaa
MattNot <matteoatnotaro@gmail.com>
2018-01-03 15:44:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { reply } = Telegraf
 const express= require('express')
 const expressApp=express();
 const PORT = process.env.PORT || 3000;
-const API_TOKEN="496154640:AAHxzqJIYglX6yVxArS4PpYpOFKongtFqlU";
-const URL = process.env.URL || 'https://guarded-sands-12822.herokuapp.com/'
+const API_TOKEN="";
+const URL = process.env.URL || ''
 const Cron= require('cron').CronJob
 var json=[];
 var crono=[];
@@ -15,7 +15,7 @@ bot.telegram.getMe().then((bot_info)=>{
 })
 var http = require("http");
 setInterval(function() {
-    http.get("http://guarded-sands-12822.herokuapp.com");
+    http.get("");
 }, 300000); // every 5 minutes (300000)
 
 expressApp.use(bot.webhookCallback(`/bot${API_TOKEN}`));
@@ -27,9 +27,6 @@ bot.command('help', (ctx) => {
 	ctx.reply("To watch a round type \"Watch <battleId>\"")
 })
 bot.hears('Ciao', (ctx) => ctx.reply('Hey bello!'))
-bot.hears('Albe', (ctx)=>ctx.reply("nabbo"))
-bot.hears('Alce', (ctx)=>ctx.reply("gay"))
-bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'))
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears(/Watch (.+)/, 
 	(match)=>
